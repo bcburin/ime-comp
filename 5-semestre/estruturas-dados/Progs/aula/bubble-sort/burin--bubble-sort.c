648222efffa223480ae11067934c95d4b67e6c6f
Bruno <cburin.bruno@gmail.com>
2022-02-27 16:43:56 -0300

    Atualizar algoritmo do bubble sort

--- 5-semestre/estruturas-dados/Progs/aula/bubble-sort/burin--bubble-sort.c
+++ 5-semestre/estruturas-dados/Progs/aula/bubble-sort/burin--bubble-sort.c
@@ -8,15 +8,18 @@ void switch_ints(int *a, int *b);
 /* Imprimir array */
 void print_array(int *arr, int size, char *nome);
 
+
+/* Imprime erro na tela caso nao seja passado o numero correto de argumentos ao progrma 
+ * Recebe como argumento o nome do programa
+ */
+int print_error(char *name);
+
+
+/* MAIN */
 int main(int argc, char *argv[]) {
   // Verificar existencia de argumentos
-  if (argc == 1) {
-    printf("\n-------------------------------------------------------------\n");
-    printf("Insira os inteiros a serem ordenados na linha de comando\n");
-    printf("Exemplo: bubble-sort 5 4 6 8 7 1\n");
-    printf("-------------------------------------------------------------\n");
-    return 1;
-  }
+  if (argc == 1)
+    return print_error(argv[0]);
 
   // Alocar espaco na memoria
   int *array = (int*) malloc( (argc-1)*sizeof(int) );
@@ -41,6 +44,7 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+
 void switch_ints(int *a, int *b) {
   int temp = *a;
   *a = *b;
@@ -56,10 +60,25 @@ void bubble_sort(int *arr, int size) {
   }
 }
 
+
 void print_array(int *arr, int size, char *nome) {
   printf("\n%s: ", nome);
   for (int i = 0; i != size; ++i) {
     printf("%d ", arr[i]);
   }
   printf("\n");
+}
+
+
+int print_error(char *name) {
+  // Retirar o diretorio do nome do executavel
+  for(char *trav = name; *trav != 0; ++trav)
+    if (*trav == '\\' || *trav == '/') name = trav+1;
+
+  // Imprimir erro com base no nome do executavel
+  printf("\n-------------------------------------------------------------\n");
+  printf("Insira os inteiros a serem ordenados na linha de comando\n");
+  printf("Exemplo: %s 25 15 20 30 25\n", name);
+  printf("-------------------------------------------------------------\n");
+  return 1;
 }
