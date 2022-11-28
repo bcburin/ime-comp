335c4cc5be9f3fb5dbe33a9b507ae8da24ee7f01
bcburin <cburin.bruno@gmail.com>
2022-11-27 22:54:07 -0300

    Correção: ex6 lista prolog 2022-11-27

--- 6-semestre/logica/progs/lista-prolog/ex1.pl
+++ 6-semestre/logica/progs/lista-prolog/ex1.pl
@@ -5,13 +5,21 @@ member(X,[X|_]).
 member(X,[_|T]) :- member(X,T).
 
 
+/* Removes first occurrence of element in list */
+
+remove(_, [], []).
+remove(X, [X|R], R).
+remove(X, [H|T], [H|R]) :- remove(X, T, R).
+
+
 /* Finds intersection of lists with repetition */
 
 inter_rep([],_,[]).
 
 inter_rep([X|T],L,[X|I]) :-
     member(X,L),
-    inter_rep(T,L,I).
+    remove(X,L,U),
+    inter_rep(T,U,I).
 
 inter_rep([X|T],L,I) :-
     \+ member(X,L),
