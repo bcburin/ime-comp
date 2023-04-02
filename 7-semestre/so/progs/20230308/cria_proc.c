7cd45b49ea52c85c5b9e96b841b6679c38c5ad23
Bruno Burin <cburin.bruno@gmail.com>
2023-04-02 14:11:54 -0300

    vault backup: 1 files updated on 2023-04-02 14:11:54
    Affected files:
    7-semestre/so/progs/20230308/cria_proc.c

--- 7-semestre/so/progs/20230308/cria_proc.c
+++ 7-semestre/so/progs/20230308/cria_proc.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
+#define PROG_PATH "/path/para/executavel"
 
 int main(int argc, char **argv) {
-  int pid;
-  /* cria outro processo */
-  pid = fork();
-  if (pid < 0) { /* ocorreu erro na execução do Fork */
-    fprintf(stderr, "Falha no Fork\n");
-    exit(-1);
-  }
-  else if (pid == 0) { /* processo filho */
-    execlp("~/IME/7-semestre/OS/loop","loop",NULL);
-    exit(1);
-  }
-  else { /* processo pai */
-    wait(NULL); /* pai espera o término do filho */
-    printf("Filho terminou\n");
-    exit(0);
-  }
+    int pid;
+    /* cria outro processo */
+    pid = fork();
+    if (pid < 0) { /* ocorreu erro na execução do Fork */
+        fprintf(stderr, "Falha no Fork\n");
+        exit(-1);
+    } else if (pid == 0) { /* processo filho */
+        execlp(PROG_PATH, "loop", NULL);
+        exit(1);
+    } else {        /* processo pai */
+        wait(NULL); /* pai espera o término do filho */
+        printf("Filho terminou\n");
+        exit(0);
+    }
 }
